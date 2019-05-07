9b7360460322c8d45d295ccc3b82b370928e00d1
Chong Han Chua <johncch@outlook.com>
2019-05-07 09:32:17 -0700

    Making it possible to run MongoDB backend locally
    This is what I did:
    * Added the db snapshot into the repository
    * Upgraded react-scripts
    * Wrote a small proxy for the cleint service
    * New scripts in package.json in the backend service to boostrap db
    * READMEs that describes the work

--- client/client/src/components/AllProjects.js
+++ client/client/src/components/AllProjects.js
@@ -1,43 +1,45 @@
-import React, { Component } from 'react';
-import ProjectCard from './ProjectCard'
+import React, { Component } from "react";
+import ProjectCard from "./ProjectCard";
 
 class AllProjects extends Component {
-
   constructor(props) {
-   super(props);
-   this.handleClick = this.handleClick.bind(this);
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
 
-   this.state = {
-     data: null,
-   };
- }
-
- componentDidMount() {
-   fetch('/projects')
-     .then(response => response.json())
-     .then(data => this.setState({ data }));
- }
-
- handleClick(){
-   this.props.history.push('/create_new_project/');
- }
-
- render() {
-    if(this.state.data != null){
-    return(
-      <div className="AllProjects">
-        <div className="AllProjectsHeader">
-          <h1>All Projects</h1>
-          <button onClick={this.handleClick}className="NewButton"> + Create New</button>
-        </div>
-        {this.state.data.map(function(project, i){
-          return <ProjectCard data={project} key={i} />;
-        })}
-      </div>
-    );
+    this.state = {
+      data: null
+    };
   }
 
-  return null;
+  componentDidMount() {
+    fetch("/projects")
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+
+  handleClick() {
+    this.props.history.push("/create_new_project/");
+  }
+
+  render() {
+    if (this.state.data != null) {
+      return (
+        <div className="AllProjects">
+          <div className="AllProjectsHeader">
+            <h1>All Projects</h1>
+            <button onClick={this.handleClick} className="NewButton">
+              {" "}
+              + Create New
+            </button>
+          </div>
+          {this.state.data.map(function(project, i) {
+            return <ProjectCard data={project} key={i} />;
+          })}
+        </div>
+      );
+    }
+
+    return null;
   }
 }
 
---
+++ client/client/src/setupProxy.js
@@ -0,0 +1,24 @@
+const proxy = require("http-proxy-middleware");
+
+// [johncch] We want to preserve Hot Module Reloading while not ejecting
+// from [create-react-app](https://facebook.github.io/create-react-app/docs/available-scripts#npm-run-eject)
+// To do this, we need a more complex configuration, so that all HTML page
+// react requests and the HMR stuff goes to the web server proxy, while all
+// other requests goes to the backend. At least until we eject and build.
+//
+// See where this came from: https://facebook.github.io/create-react-app/docs/proxying-api-requests-in-development
+// const url = "http://localhost:3001";
+const url = "http://18.188.32.120:3001";
+const apiUrlFragments = [
+  "/projects",
+  "/ideas",
+  "/prompts",
+  "/projects",
+  "/auth"
+];
+
+module.exports = function(app) {
+  apiUrlFragments.forEach(function(el) {
+    app.use(proxy(el, { target: url }));
+  });
+};
