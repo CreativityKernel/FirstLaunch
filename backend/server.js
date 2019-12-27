2bdc54a1f0e5a08b8ffa782e290d2cbe78289406
rexpiyum <rexpiyum@gmail.com>
2019-12-26 17:31:20 -0700

    fixing cannot GET issue on page refresh

--- backend/server.js
+++ backend/server.js
@@ -57,6 +57,11 @@ routes(app);
 
 let server = app.listen(port);
 
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
 const io = require("socket.io").listen(server);
 socketEvents(io);
 
